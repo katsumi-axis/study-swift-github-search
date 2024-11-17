d693d23e484c6f87bb59174bb4e18898e14648b4
katsumi <withcomputer@gmail.com>
2024-11-17 11:16:18 +0900

    Enhance CounterFeature with loading state, timer functionality, and fact retrieval

--- study-swift-github-search/ContentView.swift
+++ study-swift-github-search/ContentView.swift
@@ -5,72 +5,137 @@
 //  Created by axis on 2024/11/17.
 //
 
-import SwiftUI
 import ComposableArchitecture
-
+import SwiftUI
 
 @Reducer
 struct CounterFeature {
-  @ObservableState
-  struct State {
-    var count = 0
-  }
-  
-  enum Action {
-    case decrementButtonTapped
-    case incrementButtonTapped
-  }
-  
-  var body: some ReducerOf<Self> {
-    Reduce { state, action in
-      switch action {
-      case .decrementButtonTapped:
-        state.count -= 1
-        return .none
-        
-      case .incrementButtonTapped:
-        state.count += 1
-        return .none
-      }
+    @ObservableState
+    struct State {
+        var count = 0
+        var fact: String?
+        var isLoading = false
+        var isTimerRunning = false
     }
-  }
+
+    enum Action {
+        case decrementButtonTapped
+        case incrementButtonTapped
+        case factButtonTapped
+        case factResponse(String)
+        case timerTick
+        case toggleTimerButtonTapped
+    }
+    enum CancelID { case timer }
+    
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .decrementButtonTapped:
+                state.count -= 1
+                return .none
+                
+            case .incrementButtonTapped:
+                state.count += 1
+                return .none
+            case .factButtonTapped:
+                state.fact = nil
+                state.isLoading = true
+                return .run { [count = state.count] send in
+                    let (data, _) = try await URLSession.shared
+                        .data(from: URL(string: "https://api.github.com/users/\(count)")!)
+                    let fact = String(decoding: data, as: UTF8.self)
+                    await send(.factResponse(fact))
+                }
+                
+            case let .factResponse(fact):
+                state.fact = fact
+                state.isLoading = false
+                return .none
+                
+            case .timerTick:
+                state.count += 1
+                state.fact = nil
+                return .none
+            case .toggleTimerButtonTapped:
+                    state.isTimerRunning.toggle()
+                    if state.isTimerRunning {
+                      return .run { send in
+                        while true {
+                          try await Task.sleep(for: .seconds(1))
+                          await send(.timerTick)
+                        }
+                      }
+                      .cancellable(id: CancelID.timer)
+                    } else {
+                      return .cancel(id: CancelID.timer)
+                    }
+            }
+            
+        }
+    }
 }
 
 struct ContentView: View {
-  let store: StoreOf<CounterFeature>
-  
-  var body: some View {
-    VStack {
-      Text("\(store.count)")
-        .font(.largeTitle)
-        .padding()
-        .background(Color.black.opacity(0.1))
-        .cornerRadius(10)
-      HStack {
-        Button("-") {
-          store.send(.decrementButtonTapped)
+    let store: StoreOf<CounterFeature>
+
+    var body: some View {
+        VStack {
+            Text("\(store.count)")
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(10)
+            HStack {
+                Button("-") {
+                    store.send(.decrementButtonTapped)
+                }
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(10)
+                
+                Button("+") {
+                    store.send(.incrementButtonTapped)
+                }
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(10)
+                Button(store.isTimerRunning ? "Stop timer" : "Start timer") {
+                        store.send(.toggleTimerButtonTapped)
+                      }
+                      .font(.largeTitle)
+                      .padding()
+                      .background(Color.black.opacity(0.1))
+                      .cornerRadius(10)
+            }
+            VStack {
+                Button("Fact") {
+                    store.send(.factButtonTapped)
+                }.font(.largeTitle)
+                    .padding()
+                    .background(Color.black.opacity(0.1))
+                    .cornerRadius(10)
+                
+                if store.isLoading {
+                  ProgressView()
+                } else if let fact = store.fact {
+                  Text(fact)
+                        .font(.callout)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                }
+
+            }
         }
-        .font(.largeTitle)
-        .padding()
-        .background(Color.black.opacity(0.1))
-        .cornerRadius(10)
-        
-        Button("+") {
-          store.send(.incrementButtonTapped)
-        }
-        .font(.largeTitle)
-        .padding()
-        .background(Color.black.opacity(0.1))
-        .cornerRadius(10)
-      }
     }
-  }
 }
 
 #Preview {
     ContentView(
         store: Store(initialState: CounterFeature.State()) {
-          CounterFeature()
+            CounterFeature()
         }
     )
 }
